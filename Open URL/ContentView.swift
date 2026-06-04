978500ad51f9a1a3998ba97626dbb11310a4d9aa
Nguyễn Huỳnh Anh Khoa <noreply-git@anhkhoakz.simplelogin.com>
2026-06-04 10:55:50 +0700

    Initial Commit

---
+++ Open URL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Open URL
+//
+//  Created by Nguyen Huynh Anh Khoa on 4/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Open URL/Open_URLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_URLApp.swift
+//  Open URL
+//
+//  Created by Nguyen Huynh Anh Khoa on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Open_URLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
